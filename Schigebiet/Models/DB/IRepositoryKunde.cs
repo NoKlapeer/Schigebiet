fb51d51c8f6ae2e23251af53e2116f2718581701
NoKlapeer <noklapeer@tsn.at>
2022-02-24 08:34:31 +0100

    datenbank

---
+++ Schigebiet/Models/DB/IRepositoryKunde.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schigebiet.Models.DB
+{
+    interface IRepositoryKunde
+    {
+        void Connect();
+
+        void Disconnect();
+
+
+        bool Delete(int kundenId);
+
+
+        bool Insert(Kunde kunde);
+
+
+        bool ChangeUserData(int userId);
+
+
+
+
+    }
+}
